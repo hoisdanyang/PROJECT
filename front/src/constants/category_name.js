b44396fa03a888ad96aa9ae6c5082cfd35805f41
dny1010 <psy7363@gmail.com>
2025-12-17 17:59:31 +0900

    category.jsx 수정

---
+++ front/src/constants/category_name.js
@@ -0,0 +1,47 @@
+export const CATEGORY_ORDER = {
+    dog: [
+        "feed",
+        "snack",
+        "toilet",
+        "bath",
+        "bowl",
+        "house",
+        "health",
+        "clothes",
+        "life",
+        "toy",
+        "carrier"
+    ],
+    cat: [
+        "feed",
+        "can",
+        "snack",
+        "litter",
+        "toilet",
+        "bath",
+        "bowl",
+        "house",
+        "health",
+        "clothes",
+        "life",
+        "toy",
+        "carrier"
+    ],
+    small: [],
+};
+
+export const CATEGORY_LABEL = {
+    feed: "사료",
+    can: "캔",
+    snack: "간식",
+    litter: "모래",
+    toilet: "위생/화장실",
+    bath: "미용/목욕",
+    bowl: "식기",
+    house: "하우스/울타리",
+    health: "건강관리",
+    clothes: "의류",
+    life: "실생활용품",
+    toy: "장난감",
+    carrier: "이동장"
+};
